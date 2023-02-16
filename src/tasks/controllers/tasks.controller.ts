317064fcfb7a368fedbf7c8b6df59c6c825febba
Sebastián Salas <100061338+ssalasm@users.noreply.github.com>
2023-02-16 11:35:58 -0300

    create crud & check endpoint

--- src/tasks/controllers/tasks.controller.ts
+++ src/tasks/controllers/tasks.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put, Delete } from '@nestjs/common';
 
-@Controller('tasks')
-export class TasksController {}
+@Controller('api/tasks')
+export class TasksController {
+
+    @Get()
+    findAll() {
+        return [1, 2, 3]
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number) {
+        return id;
+    }
+
+    @Post()
+    create(@Body() body: any) {
+        return body;
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() body: any) {
+        return body;
+    }
+
+    @Delete(':id')
+        delete(@Param('id') id: number) {
+            return true;
+        }
+    
+}
