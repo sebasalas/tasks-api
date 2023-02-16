f8993641941c6e8cd74d4c2ddf8f7e71cecf1c51
Sebastián Salas <100061338+ssalasm@users.noreply.github.com>
2023-02-16 13:06:40 -0300

    typeorm integration in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'sebas',
+      password: 'sebas123',
+      database: 'my_db',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10
+    }),
+    TasksModule
+  ],
   controllers: [],
   providers: [],
 })
